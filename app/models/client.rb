8e396ec5ca5d432a0fe48c6bdabe5d49f4231dbb
Brieuc Lambert <lambert.brieuc@gmail.com>
2017-03-08 19:15:41 +0100

    validations and some stuffs

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,7 @@
 class Client < ApplicationRecord
   has_many :dossiers
+  validates :nom, :prenom, :profession, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Uniquement des lettres" }
+  validates :mail, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: "Merci d'entrer un email valide" }
 end
