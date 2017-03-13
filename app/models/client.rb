740783fd93f03a835de312e173721742260b28aa
AlizeeWyart <alizee.wyart@gmail.com>
2017-03-13 18:07:01 +0100

    feature/validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,7 +4,7 @@ class Client < ApplicationRecord
   # validates :nom, :prenom, :profession
   validates :mail, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
     message: "Merci d'entrer un email valide" }
-  validates :mail, :prenom, :nom, :profession, :date_de_naissance, :categorie, :nationalite, :mail, :portable, :adresse, presence: true
+  validates :mail, :prenom, :nom, :date_de_naissance, :categorie, :portable, presence: true
   CATEG = ["Particulier", "Association", "Entreprise", "Libéral"]
   NATIONS = [["Française", "france"], ["Italienne", "italy"], ["Anglaise", "united-kingdom"], ["Américaine", "united-states-of-america"], ["Allemande","germany"], ["Espagnole", "spain"], ["Suisse", "switzerland"], ["Australienne", "australia"], ["Belge", "belgium"], ["Canadienne", "canada"], ["Des Îles Cocos","cocos-islands"], ["Danoise", "denmark"], ["Grècque","greece"], ["Japonaise", "japan"], ["Luxembourgeois", "luxembourg"], ["Monégasque", "monaco"], ["Norvégien", "norway"], ["Portugais", "portugal"], ["Russe", "russia"], ["Thaïlandais", "thailand"], ["Turque", "turkey"]]
 end
